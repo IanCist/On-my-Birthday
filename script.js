aedaa456bcddfadb5711d936796aaadae9871596
IanCist <144719417+IanCist@users.noreply.github.com>
2024-01-31 02:07:58 +0000

    added another event

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 
 
     function changeDate1173() {
-document.getElementById("image1").src = "pisa.jpg"
-document.getElementById("text1").textContent = "THIS IS ALL ABOUT THIS NEW DTTE"
+document.getElementById("image1").src = "pisa2construction.jpg"
+document.getElementById("text1").textContent = "On August 9th 1173 the construction was started on the leaning tower of Pisa. "
 
     }
+    function changeDate1483(){ 
+        document.getElementById("image2").src = "sistinechapel.jpeg"
+document.getElementById("text2").textContent = "On August 9th 1173 the construction was started on the leaning tower of Pisa. "
+
+    }
+    
