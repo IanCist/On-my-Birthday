ccbe6cba098cba5b333ecd9ab59701915a04aef8
IanCist <144719417+IanCist@users.noreply.github.com>
2024-02-02 02:56:48 +0000

    finished project

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
-
+function changeDate2007(){ 
+    document.getElementById("image1").src = "IMG_3159.JPG"
+document.getElementById("text1").textContent = "Hi my name is Ian Cisternas! Welcome to my website dedicated to the significant events that unfolded on my birthday August 9th throughout history. I was born on August 9th, 2007, in Summit NJ, I am argentinian and chilean and have one younger sister who is 11. Now sit back and relax. Enjoy!"
+}
     function changeDate1173() {
 document.getElementById("image1").src = "pisa2construction.jpg"
-document.getElementById("text1").textContent = "On August 9th 1173 the construction was started on the leaning tower of Pisa. "
+document.getElementById("text1").textContent = "The leaning Tower of Pisa was a freestanding bell tower. It took 200 years to build and stands 56km tall. Work started in 1173 and finished in 1372. Its unintended tilt resulted from a foundation settling unevenly on soft soil. "
 
     }
     function changeDate1483(){ 
@@ -15,5 +18,19 @@ document.getElementById("text1").textContent = "On August 9th 1483 the Sistine c
 document.getElementById("text1").textContent = "On August 9th 1841 the Erie steamboad that served as a passenger ship caught fire and sank resulting in the death of 254 people"
 
     }
+
+    function changeDate1945(){ 
+        document.getElementById("image1").src = "nagasakibombing.png"
+document.getElementById("text1").textContent = "On August 9th 1945 the US droped an atomic bomb named the Fat Man on the Japanese cit of Nagasaki. The devastating attack resulted in widespread destruction, causing immediate and long-term consequences. The bombing of Nagasaki played a crucial role in Japan's decision to surrender, contributing to the end of World War II. "
+
+    }
+
+    function changeDate1974(){ 
+        document.getElementById("image1").src = "presnixon.jpeg"
+document.getElementById("text1").textContent = "On August 9th 1974 President Nixon ended his term as the 37th president of the United States. Minutes later President Ford was sworn in as 38th president of the United States. President Nixon was the first U.S president to resign."
+
+    }
+    
+    
     
     
